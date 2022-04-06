0f97ea96074804b04a66b6e70be77b44605cccbe
Akash Srivastava <akashniki@gmail.com>
2022-04-06 20:04:19 +0200

    project: Refactor further and split it into modules
    * Lets split the functions into modules. This way we split the functionality of the game and this also follows our class
    diagram

--- sourcecode/scripts/modules/tetris.js
+++ sourcecode/scripts/modules/tetris.js
@@ -1,4 +1,4 @@
-class Tetris {
+export default class Tetris {
     constructor() {
         this.grid = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -257,97 +257,5 @@ class Tetris {
         };
         this.bagindex = 2;
         this.movesTaken = 0;
-    }
-}
-
-function move(tetris) {
-    tetris.MoveDown();
-}
-
-let speed = 500;
-
-let tetris = new Tetris();
-let keyHandler = (k) => {
-    if (k.keyCode === 40) {
-        tetris.MoveDown();
-    } else if (k.keyCode === 37) {
-        tetris.MoveLeft();
-    } else if (k.keyCode === 39) {
-        tetris.MoveRight();
-    } else if (k.keyCode === 38) {
-        tetris.Rotate();
-    } else if (k.key === "s") {
-        clearInterval(id2)
-        speed -= 50;
-        id2 = setInterval(move, speed, tetris);
-    }
-}
-
-function startGame() {
-    clearInterval(id2)
-    id2 = setInterval(move, 500, tetris);
-    //console.log(id2)
-}
-
-function resetGame() {
-    clearInterval(id2)
-    speed = 500;
-    tetris.Reset();
-}
-
-function pauseGame() {
-    clearInterval(id2);
-    //console.log(id2)
-}
-
-document.addEventListener("keydown", keyHandler);
-setInterval(print, 100, tetris);
-let scorebord = document.getElementById("scoreboard");
-let upcomingShape = document.getElementById("upcomingShape");
-tetris.ApplyShape();
-let id2;
-
-function print(tetris) {
-    let grid = document.getElementById("tetris");
-    let e = document.getElementById("tetris");
-    let child = e.lastElementChild;
-    while (child) {
-        e.removeChild(child);
-        child = e.lastElementChild;
-    }
-    for (let y = 0; y < 20; y++) {
-        let zin = document.createElement("p");
-        for (let x = 0; x < 10; x++) {
-            let letter = document.createElement("span");
-            letter.style.color = tetris.colors[tetris.grid[y][x]];
-            letter.textContent += tetris.grid[y][x];
-            zin.appendChild(letter);
-        }
-        grid.appendChild(zin);
-    }
-    scorebord.style.color = "#FFFFFF";
-    scorebord.textContent = tetris.score;
-    let letter;
-    child = upcomingShape.lastElementChild;
-    while (child) {
-        upcomingShape.removeChild(child);
-        child = upcomingShape.lastElementChild;
-    }
-    for (let y = 0; y < Object.values(tetris.upcomingShape.shape)[0].length; y++) {
-        let zin = document.createElement("p");
-        for (let x = 0; x < Object.values(tetris.upcomingShape.shape)[0][0].length; x++) {
-            letter = document.createElement("span");
-            let waarde = Object.values(tetris.upcomingShape.shape)[0][y][x];
-            if (waarde !== 0) {
-                letter.style.color = tetris.colors[waarde];
-                letter.textContent = waarde;
-                zin.appendChild(letter);
-            } else {
-                letter.textContent = waarde;
-                letter.style.visibility = "hidden";
-                zin.appendChild(letter);
-            }
-        }
-        upcomingShape.appendChild(zin);
     }
 }
