4ac629328d82fcf8483f7c5eea7182fbd2a73f2b
Akash Srivastava <akashniki@gmail.com>
2022-04-06 19:29:42 +0200

    Tetris: Refactor code and add some features
    * Refactor the whole code to make it readable
    * Added start, pause and reset function
    * Updated UI according to it
    * Fixed some typos etc

--- sourcecode/scripts/tetris.js
+++ sourcecode/scripts/tetris.js
@@ -1,95 +1,129 @@
-class Tetris{
+class Tetris {
     constructor() {
-        this.grid = [[0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0]]
+        this.grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
         this.shapes = {
-            "L":[[0,0,1],
-                [1,1,1]],
-            "J":[[2,0,0],
-                [2,2,2]],
-            "I":[[3,3,3,3]],
-            "O":[[4,4],
-                [4,4]],
-            "S":[[0,5,5],
-                [5,5,0]],
-            "T":[[0,6,0],
-                [6,6,6]],
-            "Z":[[7,7,0],
-                [0,7,7]]
+            "L": [
+                [0, 0, 1],
+                [1, 1, 1]
+            ],
+            "J": [
+                [2, 0, 0],
+                [2, 2, 2]
+            ],
+            "I": [
+                [3, 3, 3, 3]
+            ],
+            "O": [
+                [4, 4],
+                [4, 4]
+            ],
+            "S": [
+                [0, 5, 5],
+                [5, 5, 0]
+            ],
+            "T": [
+                [0, 6, 0],
+                [6, 6, 6]
+            ],
+            "Z": [
+                [7, 7, 0],
+                [0, 7, 7]
+            ]
         }
-        this.colors = ["#FFFFFF","#FF0000","#00FF00","#0000FF","#FFA500","#FFFF00","#FF007F","#6A0DAD"];
+        this.colors = ["#FFFFFF", "#FF0000", "#00FF00", "#0000FF", "#FFA500", "#FFFF00", "#FF007F", "#6A0DAD"];
         this.bag = [];
         this.GenerateBag();
         this.score = 0;
-        this.currentShape = {x: 3, y: 0, shape: this.bag[0]};
-        this.upcommingShape = {x: 3, y: 0, shape: this.bag[1]};
+        this.currentShape = {
+            x: 3,
+            y: 0,
+            shape: this.bag[0]
+        };
+        this.upcomingShape = {
+            x: 3,
+            y: 0,
+            shape: this.bag[1]
+        };
         this.ai = false;
         this.bagindex = 2;
         this.movesTaken = 0;
     }
     //genereren van de set van shapes die gebruikt worden, aangezien er maar 500 moves mogen worden gemaakt loopt de forlus tot 500.
-    GenerateBag(){
+    GenerateBag() {
         let random;
         let y = 0;
-        for(let i = 0;i<500;i++){
-            random = Math.floor(Math.random()*7);
+        for (let i = 0; i < 500; i++) {
+            random = Math.floor(Math.random() * 7);
             y = 0;
-            for(const [key, value] of Object.entries(this.shapes)){
-                if(y === random){
-                    this.bag.push({[key]: value});
+            for (const [key, value] of Object.entries(this.shapes)) {
+                if (y === random) {
+                    this.bag.push({
+                        [key]: value
+                    });
                 }
                 y++;
             }
         }
     }
     //het veranderen van de currentshape en upcomingshape.
-    NextShape(){
-        if(this.bagindex<=499){
-            this.currentShape = {x: 3, y: 0, shape: this.bag[this.bagindex]};
-            this.upcommingShape = {x: 3,y: 0, shape: this.bag[this.bagindex+1]};
+    NextShape() {
+        if (this.bagindex <= 499) {
+            this.currentShape = {
+                x: 3,
+                y: 0,
+                shape: this.bag[this.bagindex]
+            };
+            this.upcomingShape = {
+                x: 3,
+                y: 0,
+                shape: this.bag[this.bagindex + 1]
+            };
             this.bagindex++;
             this.movesTaken++;
-            if(this.Collides()){
+            if (this.Collides()) {
                 this.Reset();
             }
             this.ApplyShape();
-        }else{
+        } else {
             console.error("out of index in bag!");
         }
     }
-    ApplyShape(){//de shape in het grid steken op de juiste plaats.
-        for(let y= this.currentShape.y; y<this.currentShape.y + Object.values(this.currentShape.shape)[0].length;y++){
-            for(let x = this.currentShape.x; x<this.currentShape.x+Object.values(this.currentShape.shape)[0][0].length ;x++){
-                if(Object.values(this.currentShape.shape)[0][y-this.currentShape.y][x-this.currentShape.x] !==0){
-                    this.grid[y][x] = Object.values(this.currentShape.shape)[0][y-this.currentShape.y][x-this.currentShape.x];
+    ApplyShape() { //de shape in het grid steken op de juiste plaats.
+        for (let y = this.currentShape.y; y < this.currentShape.y + Object.values(this.currentShape.shape)[0].length; y++) {
+            for (let x = this.currentShape.x; x < this.currentShape.x + Object.values(this.currentShape.shape)[0][0].length; x++) {
+                if (Object.values(this.currentShape.shape)[0][y - this.currentShape.y][x - this.currentShape.x] !== 0) {
+                    this.grid[y][x] = Object.values(this.currentShape.shape)[0][y - this.currentShape.y][x - this.currentShape.x];
                 }
             }
         }
     }
-    MoveDown(){
+    MoveDown() {
         this.RemoveShape();
         this.currentShape.y++;
-        if(!this.Collides()){
+        if (!this.Collides()) {
             this.ApplyShape();
-        }else{
+        } else {
             this.currentShape.y--;
             this.ApplyShape();
             this.UpdateScore();
@@ -97,41 +131,41 @@ class Tetris{
         }
 
     }
-    MoveLeft(){
+    MoveLeft() {
         this.RemoveShape();
         this.currentShape.x--;
-        if (!this.Collides()){
+        if (!this.Collides()) {
             this.ApplyShape();
-        }else{
+        } else {
             this.currentShape.x++;
             this.ApplyShape();
         }
     }
-    MoveRight(){
+    MoveRight() {
         this.RemoveShape();
         this.currentShape.x++;
-        if (!this.Collides()){
+        if (!this.Collides()) {
             this.ApplyShape();
-        }else{
+        } else {
             this.currentShape.x--;
             this.ApplyShape();
         }
     }
-    Rotate(){
+    Rotate() {
         this.RemoveShape();
-        this.Transpone();
-        for(let y = 0;y<Object.values(this.currentShape.shape)[0].length;y++){
+        this.Transpose();
+        for (let y = 0; y < Object.values(this.currentShape.shape)[0].length; y++) {
             this.currentShape.shape[Object.keys(this.currentShape.shape)[0]][y].reverse();
         }
         this.ApplyShape();
     }
-    RemoveRow(y){
-        this.grid[y] = [0,0,0,0,0,0,0,0,0,0];
+    RemoveRow(y) {
+        this.grid[y] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         let it = y;
         it--;
-        while(this.grid[it] !== null && this.grid[it]!== undefined && this.grid[it] !== [0,0,0,0,0,0,0,0,0,0]){
-            for(let x = 0;x<10;x++){
-                this.grid[it+1][x] = this.grid[it][x];
+        while (this.grid[it] !== null && this.grid[it] !== undefined && this.grid[it] !== [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) {
+            for (let x = 0; x < 10; x++) {
+                this.grid[it + 1][x] = this.grid[it][x];
                 this.grid[it][x] = 0;
             }
             it--;
@@ -140,18 +174,18 @@ class Tetris{
 
     Collides() {
         let overlap = false;
-        for(let y = 0;y<Object.values(this.currentShape.shape)[0].length;y++){
-            for(let x = 0; x<Object.values(this.currentShape.shape)[0][0].length;x++){
-                if(this.currentShape.x <0 || this.currentShape.x+Object.values(this.currentShape.shape)[0][0].length >10 || this.currentShape.y+Object.values(this.currentShape.shape)[0].length>20){
+        for (let y = 0; y < Object.values(this.currentShape.shape)[0].length; y++) {
+            for (let x = 0; x < Object.values(this.currentShape.shape)[0][0].length; x++) {
+                if (this.currentShape.x < 0 || this.currentShape.x + Object.values(this.currentShape.shape)[0][0].length > 10 || this.currentShape.y + Object.values(this.currentShape.shape)[0].length > 20) {
                     overlap = true;
                     break;
                 }
-                if(this.grid[y+this.currentShape.y][x+this.currentShape.x] !==0 &&  Object.values(this.currentShape.shape)[0][y][x] !==0){
+                if (this.grid[y + this.currentShape.y][x + this.currentShape.x] !== 0 && Object.values(this.currentShape.shape)[0][y][x] !== 0) {
                     overlap = true;
                     break;
                 }
             }
-            if(overlap){
+            if (overlap) {
                 break;
             }
         }
@@ -159,88 +193,121 @@ class Tetris{
     }
 
     UpdateScore() {
-        for(let y = 0; y<20;y++){
-            if(this.grid[y].every(item => item !==0)){
+        for (let y = 0; y < 20; y++) {
+            if (this.grid[y].every(item => item !== 0)) {
                 this.RemoveRow(y);
-                this.score += 100*(20-y);
+                this.score += 100 * (20 - y);
             }
         }
     }
-    RemoveShape(){
-        for(let y= this.currentShape.y; y<this.currentShape.y + Object.values(this.currentShape.shape)[0].length;y++){
-            for(let x = this.currentShape.x; x<this.currentShape.x+Object.values(this.currentShape.shape)[0][0].length ;x++){
-                if(Object.values(this.currentShape.shape)[0][y-this.currentShape.y][x-this.currentShape.x] !== 0){
+    RemoveShape() {
+        for (let y = this.currentShape.y; y < this.currentShape.y + Object.values(this.currentShape.shape)[0].length; y++) {
+            for (let x = this.currentShape.x; x < this.currentShape.x + Object.values(this.currentShape.shape)[0][0].length; x++) {
+                if (Object.values(this.currentShape.shape)[0][y - this.currentShape.y][x - this.currentShape.x] !== 0) {
                     this.grid[y][x] = 0;
                 }
             }
         }
     }
-    Transpone(){
-        let nieuw=[];
-        for(let i=0;i<Object.values(this.currentShape.shape)[0][0].length;i++){
+    Transpose() {
+        let nieuw = [];
+        for (let i = 0; i < Object.values(this.currentShape.shape)[0][0].length; i++) {
             nieuw.push([])
-            for(let j=0;j<Object.values(this.currentShape.shape)[0].length;j++){
+            for (let j = 0; j < Object.values(this.currentShape.shape)[0].length; j++) {
                 nieuw[i].push(Object.values(this.currentShape.shape)[0][j][i]);
             }
         }
         this.currentShape.shape[Object.keys(this.currentShape.shape)[0]] = nieuw;
     }
-    Reset(){
-        this.grid = [[0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0]];
+    Reset() {
+        this.grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
         this.bag = [];
         this.GenerateBag();
         this.score = 0;
-        this.currentShape = {x: 3, y: 0, shape: this.bag[0]};
-        this.upcommingShape = {x: 3, y: 0, shape: this.bag[1]};
+        this.currentShape = {
+            x: 3,
+            y: 0,
+            shape: this.bag[0]
+        };
+        this.upcomingShape = {
+            x: 3,
+            y: 0,
+            shape: this.bag[1]
+        };
         this.bagindex = 2;
         this.movesTaken = 0;
     }
-
 }
-function move(tetris){
+
+function move(tetris) {
     tetris.MoveDown();
 }
+
+let speed = 500;
+
 let tetris = new Tetris();
-let keyHandler = (e) =>{
-    if(e.key === "s"){
+let keyHandler = (k) => {
+    if (k.keyCode === 40) {
         tetris.MoveDown();
-    }else if(e.key === "q"){
+    } else if (k.keyCode === 37) {
         tetris.MoveLeft();
-    }else if(e.key === "d") {
+    } else if (k.keyCode === 39) {
         tetris.MoveRight();
-    }else if(e.key === " "){
+    } else if (k.keyCode === 38) {
         tetris.Rotate();
-    }else if(e.key === "m"){
-        id2 = setInterval(move,500,tetris);
+    } else if (k.key === "s") {
+        clearInterval(id2)
+        speed -= 50;
+        id2 = setInterval(move, speed, tetris);
     }
 }
+
+function startGame() {
+    clearInterval(id2)
+    id2 = setInterval(move, 500, tetris);
+    //console.log(id2)
+}
+
+function resetGame() {
+    clearInterval(id2)
+    speed = 500;
+    tetris.Reset();
+}
+
+function pauseGame() {
+    clearInterval(id2);
+    //console.log(id2)
+}
+
 document.addEventListener("keydown", keyHandler);
-let id = setInterval(print, 100, tetris);
-let scorebord = document.getElementById("scorebord");
-let upcommingShape = document.getElementById("upcommingShape");
+setInterval(print, 100, tetris);
+let scorebord = document.getElementById("scoreboard");
+let upcomingShape = document.getElementById("upcomingShape");
 tetris.ApplyShape();
 let id2;
 
-function print(tetris){
+function print(tetris) {
     let grid = document.getElementById("tetris");
     let e = document.getElementById("tetris");
     let child = e.lastElementChild;
@@ -248,9 +315,9 @@ function print(tetris){
         e.removeChild(child);
         child = e.lastElementChild;
     }
-    for(let y = 0;y<20; y++){
+    for (let y = 0; y < 20; y++) {
         let zin = document.createElement("p");
-        for(let x = 0;x<10;x++){
+        for (let x = 0; x < 10; x++) {
             let letter = document.createElement("span");
             letter.style.color = tetris.colors[tetris.grid[y][x]];
             letter.textContent += tetris.grid[y][x];
@@ -261,26 +328,26 @@ function print(tetris){
     scorebord.style.color = "#FFFFFF";
     scorebord.textContent = tetris.score;
     let letter;
-    child = upcommingShape.lastElementChild;
+    child = upcomingShape.lastElementChild;
     while (child) {
-        upcommingShape.removeChild(child);
-        child = upcommingShape.lastElementChild;
+        upcomingShape.removeChild(child);
+        child = upcomingShape.lastElementChild;
     }
-    for(let y =0;y < Object.values(tetris.upcommingShape.shape)[0].length;y++){
+    for (let y = 0; y < Object.values(tetris.upcomingShape.shape)[0].length; y++) {
         let zin = document.createElement("p");
-        for(let x = 0;x<Object.values(tetris.upcommingShape.shape)[0][0].length;x++){
+        for (let x = 0; x < Object.values(tetris.upcomingShape.shape)[0][0].length; x++) {
             letter = document.createElement("span");
-            let waarde = Object.values(tetris.upcommingShape.shape)[0][y][x];
-            if(waarde !==0){
+            let waarde = Object.values(tetris.upcomingShape.shape)[0][y][x];
+            if (waarde !== 0) {
                 letter.style.color = tetris.colors[waarde];
-                letter.textContent =waarde;
+                letter.textContent = waarde;
                 zin.appendChild(letter);
-            }else{
+            } else {
                 letter.textContent = waarde;
                 letter.style.visibility = "hidden";
                 zin.appendChild(letter);
             }
         }
-        upcommingShape.appendChild(zin);
+        upcomingShape.appendChild(zin);
     }
 }
