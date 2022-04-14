f4f022c9df81f2f6aa97e1675b41656a9bd29c9a
Akash Srivastava <akashniki@gmail.com>
2022-04-14 18:48:35 +0200

    tetris: Add sound effect
    * We added sound effect for various parts of the game:
      - Sound effect is played when moving the tetrix to left, right or while rotating it.
      - Also when you star, play, pause the game.
    
    * While at it, sort the main.js aswell

--- sourcecode/scripts/main.js
+++ sourcecode/scripts/main.js
@@ -1,27 +1,37 @@
 import Tetris from "./modules/tetris.js"
+import Sound from "./modules/sound.js"
 
 //alert("Script detected")
 
-let canvas = document.getElementById("board")
-let ctx = canvas.getContext("2d")
-
+// Create canvas
 let COLS = 10
 let ROWS = 20
 let BLOCK_SIZE = 45
+
+let canvas = document.getElementById("board")
+let ctx = canvas.getContext("2d")
 
 ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
 
-let tetris = new Tetris();
+// Start of sound effect settings
+let sound = new Sound(document.getElementById("sound-div")),
+    // Create 5 sound effects: Buttons (Play, Pause, Reset), Rotate, MoveLeft == MoveRight, GameOver, BackgroundMusic
+    buttonSound = sound.create("assets/sounds/block-rotate.mp3", "button_sound"),
+    rotateSound = sound.create("assets/sounds/select.mp3", "rotate_sound"),
+    moveSound = sound.create("assets/sounds/whoosh.mp3", "move_sound"),
+    gameOverSound = sound.create("assets/sounds/gameover.mp3", "gameOver_sound"),
+    backgroundMusic = sound.create("assets/sounds/pause.mp3", "backgroundMusic");
 
-function move(tetris) {
-    tetris.MoveDown();
-}
+sound.MuteToggle();
+sound.SoundSettings();
 
+// Functions to handle various keypresses from keyboard
 let speed = 500;
 let play = false;
+let id2;
 
 let keyHandler = (k) => {
     if(play){
@@ -29,10 +39,13 @@ let keyHandler = (k) => {
             tetris.MoveDown();
         } else if (k.keyCode === 37) {
             tetris.MoveLeft();
+            moveSound.play();
         } else if (k.keyCode === 39) {
             tetris.MoveRight();
+            moveSound.play();
         } else if (k.keyCode === 38) {
             tetris.Rotate();
+            rotateSound.play();
         } else if (k.key === "s") {
             clearInterval(id2)
             speed -= 50;
@@ -43,10 +56,15 @@ let keyHandler = (k) => {
     }
 }
 
+document.addEventListener("keydown", keyHandler);
+
+// Various functions to Star, Pause and Reset the game
 function startGame() {
     clearInterval(id2);
     id2 = setInterval(move, 500, tetris);
     play = true;
+    buttonSound.play();
+    backgroundMusic.play();
 }
 
 function resetGame() {
@@ -54,25 +72,33 @@ function resetGame() {
     speed = 500;
     tetris.Reset();
     play = false;
+    buttonSound.play();
 }
 
 function pauseGame() {
     clearInterval(id2);
     //console.log(id2)
     play = false;
+    buttonSound.play();
 }
 
 document.getElementById("startButton").addEventListener("click", startGame)
 document.getElementById("pauseButton").addEventListener("click", pauseGame)
 document.getElementById("resetButton").addEventListener("click", resetGame)
 
-document.addEventListener("keydown", keyHandler);
+// Create a new Tetris game
+let tetris = new Tetris();
 
 let printinterval = setInterval(print, 100, tetris);
+
 let scorebord = document.getElementById("scoreboard");
 let upcomingShape = document.getElementById("upcomingShape");
+
 tetris.ApplyShape();
-let id2;
+
+function move(tetris) {
+    tetris.MoveDown();
+}
 
 function print(tetris) {
 
