ff411f955a359b98c8b79478986634ad7f6da87b
Akash Srivastava <akash.srivastava@ugent.be>
2022-05-12 19:07:19 +0200

    project: main: graph: Show average moves per generation +  fix reset button bug
    * Also fix a but where the graph won't reset when clicking on the reset button

--- sourcecode/scripts/main.js
+++ sourcecode/scripts/main.js
@@ -211,6 +211,10 @@ function resetGame() {
     play_sound = true;
     buttonSound.play();
     vorigeScore = 0;
+    graphData.data.datasets[0].data = [];
+    graphData.data.labels = [];
+    chart.destroy();
+    refreshChart();
     gameOverScreen.setAttribute("visibility", "hidden")
 }
 
@@ -507,20 +511,15 @@ const refreshChart = () => {
 
 const handleRandomDataset = () => {
     if (tetris.ai_activated) {
-            let bla = ai.moves.reduce(function (a, b) {
-                return Math.max(a, b);
-            })
-            graphData.data.datasets[0].data.push(bla);
-            graphData.data.labels.push(ai.populationNumber);
+
+        let bla = ai.moves.reduce(function (a, b) {
+            return a + b;
+        })
+        graphData.data.datasets[0].data.push(bla/ai.populationSize);
+        graphData.data.labels.push(ai.populationNumber);
     }
     chart.destroy();
     refreshChart();
-};
-
-// util
-// random function
-const getRandomInt = () => {
-   return Math.floor(Math.random() *  20);
 };
 // End of graph
 
