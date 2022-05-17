deca409f5e6eefe7f5be0eeffdc294eb0c6173fe
Tomas Galle <tomas.galle@ugent.be>
2022-05-17 10:58:35 +0200

    some refactoring

--- sourcecode/scripts/translator.js
+++ sourcecode/scripts/translator.js
@@ -1,57 +1,55 @@
 let defaultLocale = localStorage.getItem("locale");
-
-let locale;
+let currentLocale;
 
 let translations = {};
 
+//Initialise locale and translate page when loaded
 document.addEventListener("DOMContentLoaded", () => {
-
     if (defaultLocale === null) {
         localStorage.setItem("locale", "en");
-        defaultLocale = localStorage.getItem("locale");
+        defaultLocale = "en";
     }
-
     setLocale(defaultLocale);
-
     bindLocaleSwitcher(defaultLocale);
 });
 
+//Switch locale
 async function setLocale(newLocale) {
-    if (newLocale === locale) return;
-
-    const newTranslations =
-        await fetchTranslationsFor(newLocale);
-
-    locale = newLocale;
+    if (newLocale === currentLocale) return;
+    let newTranslations =
+        await getTranslationsFor(newLocale);
+    currentLocale = newLocale;
     localStorage.setItem("locale", newLocale);
     translations = newTranslations;
-
-    translate_page();
+    translatePage();
 }
 
-async function fetchTranslationsFor(newLocale) {
-    const response = await fetch(`assets/lang/${newLocale}.json`);
+//Get translations for a locale
+async function getTranslationsFor(newLocale) {
+    let response = await fetch(`assets/lang/${newLocale}.json`);
     return await response.json();
 }
 
-function translate_page() {
+//Translate all HTML elements containing a translator key
+function translatePage() {
     document
         .querySelectorAll("[data-i18n-key]")
         .forEach(translateElement);
 }
 
+//Translate content of an HTML element
 function translateElement(element) {
-    const key = element.getAttribute("data-i18n-key");
+    let key = element.getAttribute("data-i18n-key");
     let translation = translations[key];
-    if (translation[0] === '"') {
+    //If text is surrounded by double quotes, don't show these in page
+    if (translation[0] === '"')
         translation = translation.substring(1, translation.length - 1);
-    }
-    //console.log(`${translation}`)
     element.innerText = translation;
 }
 
+//Add functionality to the locale selector
 function bindLocaleSwitcher(initialValue) {
-    const switcher =
+    let switcher =
         document.querySelector("[data-i18n-switcher]");
     switcher.value = initialValue;
     switcher.onchange = (e) => {
